776dd9e019df347faa8a0941b1d337ea1ceb7efb
petar111 <jpetar765@gmail.com>
2023-10-19 10:10:34 +0200

    Initialized heroes config server project.

---
+++ src/main/java/com/springpj/heroesconfigserver/HeroesconfigserverApplication.java
@@ -0,0 +1,13 @@
+package com.springpj.heroesconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroesconfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroesconfigserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springpj/heroesconfigserver/HeroesconfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springpj.heroesconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroesconfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
